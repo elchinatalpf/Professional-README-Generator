2aa84e9e6c73cab21eb64bc6017fc7d00787686d
Javier <elchinatalpf@github.com>
2023-08-10 21:32:03 -0500

    Added prompts

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => inquirer.Prompt([
+  {
+    type: 'input',
+    name: 'name',
+    message: 'what is the title of your project?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Describe the purpuso of your project:',
+  },
+  {
+    type: 'checkbox',
+    name: 'stack',
+    message: 'Select a license for your project:',
+    choices: ['MIT', 'ISC', 'JavaScript', 'Spanish'],
+  },
+]);
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -11,3 +31,12 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+
+// .gitignore: done
+// npm init and i inquirer@8.2.4: done
+// app video and others screen shots:
+
+// What is the title of your project?
+// Describe the purpose of your project:
+// Select a license for your project: (apache, boots, bsd, 
